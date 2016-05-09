7690a016a106fb66eff9fac5e47e411a8db51fb1
Miguel Inostroza <miguelinostroza3@gmail.com>
2016-05-09 19:15:29 -0300

    Aqui el desarrollo del reto

--- reto.playground/Contents.swift
+++ reto.playground/Contents.swift
@@ -2,4 +2,18 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var rango = 1...100
+
+for i in rango {
+    
+    if (i % 5 == 0){
+        print("\(i) Bingo")
+    }else if (i % 2 == 0){
+        print("\(i) Par")
+    }else if (i % 3 == 0){
+        print("\(i) Inpar")
+    }else if (i > 30 && i < 40){
+        print ("\(i) Viva Swift")
+    }
+    
+}
