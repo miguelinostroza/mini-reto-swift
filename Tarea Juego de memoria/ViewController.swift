b8ad0566ad9861141117cdbcf0b94b22ea49b04a
Miguel Inostroza <miguelinostroza3@gmail.com>
2016-05-09 18:56:29 -0300

    Initial Commit

---
+++ Tarea Juego de memoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarea Juego de memoria
+//
+//  Created by Miguel Inostroza on 09-05-16.
+//  Copyright © 2016 Miguel Inostroza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tarea Juego de memoriaTests/Tarea_Juego_de_memoriaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tarea_Juego_de_memoriaTests.swift
+//  Tarea Juego de memoriaTests
+//
+//  Created by Miguel Inostroza on 09-05-16.
+//  Copyright © 2016 Miguel Inostroza. All rights reserved.
+//
+
+import XCTest
+@testable import Tarea_Juego_de_memoria
+
+class Tarea_Juego_de_memoriaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
