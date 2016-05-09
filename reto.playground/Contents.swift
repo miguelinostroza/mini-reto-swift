6ca0cf5be434aefc3e4b5a537d82019011a4d945
Miguel Inostroza <miguelinostroza3@gmail.com>
2016-05-09 19:24:20 -0300

    Entrega

--- reto.playground/Contents.swift
+++ reto.playground/Contents.swift
@@ -5,15 +5,17 @@ import UIKit
 var rango = 1...100
 
 for i in rango {
-    
-    if (i % 5 == 0){
-        print("\(i) Bingo")
-    }else if (i % 2 == 0){
-        print("\(i) Par")
-    }else if (i % 3 == 0){
-        print("\(i) Inpar")
-    }else if (i > 30 && i < 40){
-        print ("\(i) Viva Swift")
+    switch i{
+    case 30...40:
+        print(i,"Viva Swift!!!")
+    default:
+        if (i%5) == 0{
+            print("\(i) Bingo!!!")
+        } else if(i%2 == 0){
+            print("\(i) par!!!")
+        } else if(i%2 != 0){
+            print("\(i) impar!!!")
+        }
     }
     
 }
